3a4ef13f8b512d7fe0e8a56dac141a5ffb338d22
Jayachandra M <m7jayachandra@gmail.com>
2020-05-17 17:55:50 +0530

    Using a header to send the message
     Using the first 10 bytes to send the size of the message.

--- client.py
+++ client.py
@@ -1,20 +1,28 @@
 from socket import socket, AF_INET, SOCK_STREAM, gethostname
 
+HEADER_SIZE = 10
 #create a connection to the server
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((gethostname(), 1234))
+s.connect((gethostname(), 1243))
 
 def get_msg():
     full_msg = ""
+    is_new_msg = True
     while True:
-        msg = s.recv(8)
-        if len(msg) <= 0:
-            break
+        msg = s.recv(16)
+        if is_new_msg:
+            msg_len = int(msg[:HEADER_SIZE])
+            is_new_msg = False
         full_msg += msg.decode("utf-8")
-    return full_msg
+
+        if len(full_msg) - HEADER_SIZE == msg_len:
+            print(f"msg recieved... size = {len(full_msg)}")
+            print(full_msg[HEADER_SIZE:])
+            is_new_msg = True
+            full_msg = ""
+
+#    return full_msg
 
 if __name__ == "__main__":
     while True:
-        msg = get_msg()
-        if len(msg) > 0:
-            print(msg)
+        get_msg()
--- server.py
+++ server.py
@@ -1,12 +1,29 @@
 from socket import socket, AF_INET, SOCK_STREAM, gethostname
+import time
+import pickle
+
+"""
+the max size of the msg is 10000000000
+and the resulting header size is 10
+"""
+HEADER_SIZE = 10
 
 s = socket(AF_INET, SOCK_STREAM)
-s.bind((gethostname(), 1234))
+s.bind((gethostname(), 1243))
 s.listen(5)
 
 while True:
     print("Waiting for new connections...")
     client_socket, address = s.accept()
     print(f"Connection from {address} has been established...")
-    client_socket.send(bytes("Hey there!", "utf-8"))
-    client_socket.close()
+    msg = "Welcome to the server..."
+    msg = f"{len(msg):<{HEADER_SIZE}}" + msg #add the header to the msg
+    client_socket.send(bytes(msg, "utf-8"))
+
+    while True:
+        time.sleep(3)
+        msg = f"The time is {time.time()}"
+        msg = f"{len(msg):<{HEADER_SIZE}}" + msg
+        print(msg)
+        client_socket.send(bytes(msg, "utf-8"))
+   # client_socket.close()
