9585a028b5b994394ce3b8865a5b0ba79a56b27d
Jayachandra M <m7jayachandra@gmail.com>
2020-05-18 18:41:45 +0530

    Local Chat Room Version 0
    - A local server to listen for new connections, wait for new messages and broadcast the message.
    - A client to connect to the server, send and receive messages.

--- client.py
+++ client.py
@@ -1,28 +1,56 @@
-from socket import socket, AF_INET, SOCK_STREAM, gethostname
+import socket
+import select
+import errno
+import sys
 
-HEADER_SIZE = 10
-#create a connection to the server
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((gethostname(), 1243))
+HEADER_LENGTH = 10
+IP_Address = "127.0.0.1"
+Port = 1234
 
-def get_msg():
-    full_msg = ""
-    is_new_msg = True
-    while True:
-        msg = s.recv(16)
-        if is_new_msg:
-            msg_len = int(msg[:HEADER_SIZE])
-            is_new_msg = False
-        full_msg += msg.decode("utf-8")
+my_user_name = input("Username: ")
+user_name = my_user_name.encode("utf-8")
 
-        if len(full_msg) - HEADER_SIZE == msg_len:
-            print(f"msg recieved... size = {len(full_msg)}")
-            print(full_msg[HEADER_SIZE:])
-            is_new_msg = True
-            full_msg = ""
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP_Address, Port))
+client_socket.setblocking(False)
 
-#    return full_msg
+username_header = f"{len(user_name):<{HEADER_LENGTH}}".encode("utf-8")
+client_socket.send(username_header + user_name)
 
-if __name__ == "__main__":
-    while True:
-        get_msg()
+
+while True:
+    msg = input(f'{my_user_name} > ')
+
+    if msg:
+        msg = msg.encode("utf-8")
+        msg_header = f"{len(msg):<{HEADER_LENGTH}}".encode("utf-8")
+        client_socket.send(msg_header + msg)
+
+    try:
+        while True:
+            username_header = client_socket.recv(HEADER_LENGTH)
+
+            if not len(username_header):
+                print("connection lost.")
+                sys.exit()
+
+            username_length = int(username_header.decode("utf-8").strip())
+            user_name = client_socket.recv(username_length).decode("utf-8")
+
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode("utf-8").strip())
+            message = client_socket.recv(message_length).decode("utf-8")
+
+            print(f'{user_name} > {message}')
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print("Reading error: {}".format(str(e)))
+            sys.exit()
+        continue
+
+    except Exception as e:
+        print("General error: {}".format(str(e)))
+        sys.exit()
+
+
--- server.py
+++ server.py
@@ -1,29 +1,69 @@
-from socket import socket, AF_INET, SOCK_STREAM, gethostname
-import time
-import pickle
+import socket
+import select
 
-"""
-the max size of the msg is 10000000000
-and the resulting header size is 10
-"""
-HEADER_SIZE = 10
+HEADER_LENGTH = 10
+IP_Address = "127.0.0.1"
+Port = 1234
 
-s = socket(AF_INET, SOCK_STREAM)
-s.bind((gethostname(), 1243))
-s.listen(5)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP_Address, Port))
+server_socket.listen()
+
+socket_list = [server_socket]
+clients = {}
+
+print(f"Started the server on {IP_Address}:{Port}...")
+
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+
+        message_length = int(message_header.decode("utf-8").strip())
+
+        return {'header':message_header, 'data':client_socket.recv(message_length)}
+
+    except:
+        return False
+
 
 while True:
-    print("Waiting for new connections...")
-    client_socket, address = s.accept()
-    print(f"Connection from {address} has been established...")
-    msg = "Welcome to the server..."
-    msg = f"{len(msg):<{HEADER_SIZE}}" + msg #add the header to the msg
-    client_socket.send(bytes(msg, "utf-8"))
+    read_sockets, _, exception_sockets = select.select(socket_list, [], socket_list)
 
-    while True:
-        time.sleep(3)
-        msg = f"The time is {time.time()}"
-        msg = f"{len(msg):<{HEADER_SIZE}}" + msg
-        print(msg)
-        client_socket.send(bytes(msg, "utf-8"))
-   # client_socket.close()
+    for s in read_sockets:
+        if s == server_socket:
+            client_socket, client_addr = server_socket.accept()
+            user = receive_message(client_socket)
+
+            if user is False:
+                continue
+
+            socket_list.append(client_socket)
+            clients[client_socket] = user
+
+            print("New connection established from {}:{}, username: {}".format(*client_addr, user['data'].decode("utf-8")))
+
+        else:
+            msg = receive_message(s)
+
+            if msg is False:
+                print("Failed to receive the msg from: {}".format(clients[s]["data"].decode("utf-8")))
+                socket_list.remove(s)
+                del clients[s]
+                continue
+
+            user = clients[s]
+            print(f'Received msg from {user["data"].decode("utf-8")}: {msg["data"].decode("utf-8")}')
+
+            for client in clients:
+                if client != s:
+                    client.send(user["header"] + user["data"] + msg["header"] + msg["data"])
+
+    for s in exception_sockets:
+        socket_list.remove(s)
+        del clients[s]
+
