a50036c61ce273814d3452dd6883edb2a6ab2247
Jayachandra M <m7jayachandra@gmail.com>
2020-05-18 19:16:23 +0530

    Chat Room Version 0.1
    - added comments for server and client code

--- client.py
+++ client.py
@@ -1,33 +1,50 @@
+"""
+###     A client to connect to the server, introduce using the name, send and receive the messages
+###     uses a header for sending and receiving data
+###     UTF-8 encoding is used for messages
+"""
 import socket
 import select
 import errno
 import sys
 
-HEADER_LENGTH = 10
+HEADER_LENGTH = 10 #length of the msg can be 10 digits
 IP_Address = "127.0.0.1"
 Port = 1234
 
+#get user name
 my_user_name = input("Username: ")
 user_name = my_user_name.encode("utf-8")
 
+"""
+###     connect to the server
+###     AF_INET specifies the IPv4
+###     SOCK_STREAM specifies the TCP
+"""
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP_Address, Port))
 client_socket.setblocking(False)
 
+#user name is sent after connecting
 username_header = f"{len(user_name):<{HEADER_LENGTH}}".encode("utf-8")
 client_socket.send(username_header + user_name)
 
-
+#send a new message or wait for incoming messages
 while True:
-    msg = input(f'{my_user_name} > ')
+    msg = input(f'{my_user_name} > ') #new message to send
 
     if msg:
+        #if message entered, send new message
         msg = msg.encode("utf-8")
         msg_header = f"{len(msg):<{HEADER_LENGTH}}".encode("utf-8")
         client_socket.send(msg_header + msg)
 
+    #receive new messages form server in format,
+    #| username_header | user_name | message_header | message |
     try:
         while True:
+            #get full message by repeating the recv call, 
+            #if msg is empty EAGAIN or EWOULDBLOCK exception will raised
             username_header = client_socket.recv(HEADER_LENGTH)
 
             if not len(username_header):
@@ -43,14 +60,14 @@ while True:
 
             print(f'{user_name} > {message}')
 
+    #if it is an expected exception, continue the send/receive loop
     except IOError as e:
         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
             print("Reading error: {}".format(str(e)))
             sys.exit()
         continue
-
+    
+    #handle unknown exception
     except Exception as e:
         print("General error: {}".format(str(e)))
         sys.exit()
-
-
--- server.py
+++ server.py
@@ -1,55 +1,75 @@
+"""
+###     A server running on the local system listening to new connections, receive new messages and broadcast the message
+###     uses a header for sending and receiving data
+###     UTF-8 encoding is used for messages
+"""
 import socket
 import select
 
-HEADER_LENGTH = 10
+HEADER_LENGTH = 10 #length of the message can be 10 digits
 IP_Address = "127.0.0.1"
 Port = 1234
 
+"""
+###     create server
+###     AF_INET specifies the IPv4
+###     SOCK_STREAM specifies the TCP
+"""
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #to set the socket option to reuse the address
 
-server_socket.bind((IP_Address, Port))
-server_socket.listen()
+server_socket.bind((IP_Address, Port)) #binding the IP and the port
+server_socket.listen() #listen to new connections
 
-socket_list = [server_socket]
-clients = {}
+socket_list = [server_socket] #list of sockets
+clients = {} #socket to user name mapping
 
 print(f"Started the server on {IP_Address}:{Port}...")
 
+"""
+###     function to receive the messages in format,
+###     | header | data |
+"""
 def receive_message(client_socket):
     try:
-        message_header = client_socket.recv(HEADER_LENGTH)
+        #get the msg header
+        message_header = client_socket.recv(HEADER_LENGTH) 
 
+        #if header not received, client connection closed
         if not len(message_header):
             return False
 
-        message_length = int(message_header.decode("utf-8").strip())
+        #get the message length
+        message_length = int(message_header.decode("utf-8").strip()) 
 
         return {'header':message_header, 'data':client_socket.recv(message_length)}
 
     except:
         return False
 
-
 while True:
+    #get the list of sockets to be read and the list of sockets with exceptions
     read_sockets, _, exception_sockets = select.select(socket_list, [], socket_list)
 
     for s in read_sockets:
+        #if the socket is the server, then a new connection has come
         if s == server_socket:
             client_socket, client_addr = server_socket.accept()
-            user = receive_message(client_socket)
+            user = receive_message(client_socket) #receive the user name
 
             if user is False:
                 continue
 
-            socket_list.append(client_socket)
-            clients[client_socket] = user
+            socket_list.append(client_socket) #add the new socket to the list
+            clients[client_socket] = user     #create a mapping of socket to user
 
             print("New connection established from {}:{}, username: {}".format(*client_addr, user['data'].decode("utf-8")))
 
+        #new message arrived
         else:
             msg = receive_message(s)
-
+            
+            #if msg not received, something went wrong and connection lost
             if msg is False:
                 print("Failed to receive the msg from: {}".format(clients[s]["data"].decode("utf-8")))
                 socket_list.remove(s)
@@ -59,11 +79,12 @@ while True:
             user = clients[s]
             print(f'Received msg from {user["data"].decode("utf-8")}: {msg["data"].decode("utf-8")}')
 
+            #send to message to other sockets
             for client in clients:
                 if client != s:
                     client.send(user["header"] + user["data"] + msg["header"] + msg["data"])
 
+    #if there is an exception socket, something went wrong, close it
     for s in exception_sockets:
         socket_list.remove(s)
         del clients[s]
-
